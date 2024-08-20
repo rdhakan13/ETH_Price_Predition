3b000458dc2be27226fa5599f746d021c392f6fe
rdhakan13 <rdhakaan@gmail.com>
2024-08-20 21:26:13 +0100

    Merging data

--- data/python_scripts/merge_data.py
+++ data/python_scripts/merge_data.py
@@ -1,8 +1,15 @@
 #%%
 import os
 import pandas as pd
-
+# %%
+coins = ['BTC']]
 parent_dir = os.path.dirname(os.getcwd())
+ETH_data = pd.read_csv("{parent_dir}\\raw_data\\ETH-USD_price_data.csv".format(parent_dir=parent_dir))
+ETH_data['Date'] = pd.to_datetime(ETH_data['Date'])
+
+
+
+#%%
 directory = "{parent_dir}\\raw_data\\Google_News_Headlines_data\\".format(parent_dir=parent_dir)
 final_data = pd.DataFrame(columns = ['Date','News Headline','Publisher'])
 
@@ -18,8 +25,7 @@ for file in os.listdir(directory):
     final_data = pd.concat([final_data, data_unique], ignore_index=True)
     final_data = final_data.sort_values(by='Date',ascending=True)
 
-# %%
 final_data_unique = final_data.drop_duplicates(subset='News Headline', keep='first')
-# %%
+
 final_data_unique.to_csv("{parent_dir}\\cleaned_data\\Google_News_Headlines_data\\google_news_headlines_data.csv".format(parent_dir=parent_dir), index=False)
 # %%
