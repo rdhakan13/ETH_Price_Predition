d615f5bc1115d4b9a9fdc74f41e7df994bc4ad94
rdhakan13 <rdhakaan@gmail.com>
2025-03-08 13:44:31 +0000

    added plotter

---
+++ src/common/statistical_tests.py
@@ -0,0 +1,14 @@
+from statsmodels.tsa.stattools import adfuller
+
+def adf_test(series, alpha=0.05):
+    """
+    Perform Augmented Dickey-Fuller (ADF) test for stationarity.
+    
+    :param series: Pandas Series to test.
+    :param alpha: Significance level (default 0.05).
+    :return: Boolean (True if stationary, False otherwise) and test result dictionary.
+    """
+    result = adfuller(series)
+    p_value = result[1]
+    is_stationary = p_value < alpha
+    return is_stationary, {"ADF Statistic": result[0], "p-value": p_value, "Critical Values": result[4]}
