daa288fb81cf9c6eb1f5d9ccab684c0ba4c318f5
rdhakan13 <rdhakaan@gmail.com>
2025-04-13 21:09:34 +0100

    aligning code to check error

--- src/common/stats.py
+++ src/common/stats.py
@@ -22,13 +22,20 @@ def adf_test(series: pd.DataFrame, alpha:float=0.05)->StationarityTests:
     Returns:
         ADF: A named tuple containing the test result and whether the series is stationary.
     """
-    result = adfuller(series)
+    try:
+        result = adfuller(series)
+    except ValueError as e:
+        logger.error(f"Error in ADF test: {e}")
+        raise ValueError("ADF test failed. Please check the input series.")
     is_stationary = result[1] < alpha
-    return is_stationary, {
+    stationarity_result = StationarityTests()
+    stationarity_result.is_stationary = is_stationary
+    stationarity_result.result = {
         "ADF Statistic": result[0],
         "p-value": result[1],
         "Critical Values": result[4],
     }
+    return stationarity_result
 
 @ensure_annotations
 def pp_test(series: pd.DataFrame, alpha:float=0.05)->StationarityTests:
@@ -42,13 +49,21 @@ def pp_test(series: pd.DataFrame, alpha:float=0.05)->StationarityTests:
     Returns:
         dict: A dictionary containing the test result.
     """
-    pp_test = PhillipsPerron(series)
+    try:
+        pp_test = PhillipsPerron(series)
+    except ValueError as e:
+        logger.error(f"Error in PP test: {e}")
+        raise ValueError("PP test failed. Please check the input series.")
+    
     is_stationary = pp_test.pvalue < alpha
-    return is_stationary, {
+    stationarity_result = StationarityTests()
+    stationarity_result.is_stationary = is_stationary
+    stationarity_result.result = {
         "PP Statistic": pp_test.stat,
         "p-value": pp_test.pvalue,
         "Critical Values": pp_test.critical_values,
     }
+    return stationarity_result
 
 @ensure_annotations
 def grangers_causality_test(df:pd.DataFrame, target_col:str, max_lag:int=5, alpha:float=0.05)->dict:
@@ -69,8 +84,12 @@ def grangers_causality_test(df:pd.DataFrame, target_col:str, max_lag:int=5, alph
         if col == target_col:
             continue 
         
-        test_result = grangercausalitytests(df[[target_col, col]].dropna(), max_lag, verbose=False)
-    
+        try:
+            test_result = grangercausalitytests(df[[target_col, col]].dropna(), max_lag, verbose=False)
+        except Exception as e:
+            logger.error(f"Error in Granger's Causality test for {col}: {e}")
+            raise ValueError(f"Granger's Causality test failed for {col}. Please check the input data.")
+        
         p_values = {lag: test_result[lag][0]['ssr_chi2test'][1] for lag in range(1, max_lag + 1)}
         
         significant_lags = [lag for lag, p in p_values.items() if p < alpha]
