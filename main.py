4b6ef15b6c60687706ab9fd54f6c64ca89ff619c
rdhakan13 <rdhakaan@gmail.com>
2025-05-11 14:48:20 +0100

    completed sentiment_analysis stage

--- main.py
+++ main.py
@@ -10,6 +10,6 @@ if __name__ == "__main__":
     subprocess.run([PY_VENV, "src/pipeline/get_raw_data.py"], check=True)
     subprocess.run([PY_VENV, "src/pipeline/process_raw_data.py"], check=True)
     subprocess.run([PY_VENV, "src/pipeline/conduct_sentiment_analysis.py"], check=True)
-    # subprocess.run([PY_VENV, "src/pipeline/compile_final_data.py"], check=True)
+    subprocess.run([PY_VENV, "src/pipeline/compile_final_data.py"], check=True)
     # subprocess.run([PY_VENV, "src/pipeline/run_model.py"], check=True)
     # subprocess.run([PY_VENV, "src/pipeline/get_results.py"], check=True)
