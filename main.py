abdce7f18f0037698cc405c1068d21c25e8d1987
rdhakan13 <rdhakaan@gmail.com>
2025-05-16 21:44:59 +0100

    main.py finalised

--- main.py
+++ main.py
@@ -11,5 +11,5 @@ if __name__ == "__main__":
     subprocess.run([PY_VENV, "src/pipeline/process_raw_data.py"], check=True)
     subprocess.run([PY_VENV, "src/pipeline/conduct_sentiment_analysis.py"], check=True)
     subprocess.run([PY_VENV, "src/pipeline/compile_final_data.py"], check=True)
-    # subprocess.run([PY_VENV, "src/pipeline/run_model.py"], check=True)
-    # subprocess.run([PY_VENV, "src/pipeline/get_results.py"], check=True)
+    subprocess.run([PY_VENV, "src/pipeline/run_model.py"], check=True)
+    subprocess.run([PY_VENV, "src/pipeline/get_results.py"], check=True)
